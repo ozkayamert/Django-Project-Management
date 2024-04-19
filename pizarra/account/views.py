e2d57ea739701ea3ed7ee028b3c51dbb9ab8ca26
Mert Ozkaya <ozkayamert35@gmail.com>
2024-04-19 22:27:00 +0300

    sign up

--- pizarra/account/views.py
+++ pizarra/account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import User
+
+
+def signup(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        password1 = request.POST.get('password1', '')
+        password2 = request.POST.get('password2', '')
+
+        if name and email and password1 and password2:
+            user = User.objects.create_user(name, email, password1)
+
+            print('User created:', user)
+
+            return redirect('/login/')
+        else:
+            print('Something went wrong!')
+    else:
+        print('Just show the form!')
+
+    return render(request, 'account/signup.html')
