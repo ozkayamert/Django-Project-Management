734d7a3f9a59605f71788b8a8285cf65ec7b014f
Mert Ozkaya <ozkayamert35@gmail.com>
2024-04-20 21:18:40 +0300

    Add todolist

--- pizarra/todolist/views.py
+++ pizarra/todolist/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Todolist
+from project.models import Project
+
+def add(request, project_id):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+
+        if name:
+            Todolist.objects.create(project=project, name=name, description=description, created_by=request.user)
+
+            return redirect(f'/projects/{project_id}/')
+
+    return render(request, 'todolist/add.html', {
+        'project': project
+    })
