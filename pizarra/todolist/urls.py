e5cbd0a8827108a96ee5b76e841543674288a120
Mert Ozkaya <ozkayamert35@gmail.com>
2024-04-20 21:52:28 +0300

    Edit todolist

--- pizarra/todolist/urls.py
+++ pizarra/todolist/urls.py
@@ -7,4 +7,5 @@ app_name = 'todolist'
 urlpatterns = [
     path('add/', views.add, name='add'),
     path('<uuid:pk>/', views.todolist, name='todolist'),
+    path('<uuid:pk>/edit/', views.edit, name='edit'),
 ]
--- pizarra/todolist/views.py
+++ pizarra/todolist/views.py
@@ -30,3 +30,25 @@ def add(request, project_id):
     return render(request, 'todolist/add.html', {
         'project': project
     })
+
+
+@login_required
+def edit(request, project_id, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    todolist = Todolist.objects.filter(project=project).get(pk=pk)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+
+        if name:
+            todolist.name = name
+            todolist.description = description
+            todolist.save()
+
+            return redirect(f'/projects/{project_id}/{pk}/')
+
+    return render(request, 'todolist/edit.html', {
+        'project': project,
+        'todolist': todolist
+    })
