76e7e26ec13ac472b2bbfbc6d2688455ac2c30a8
Mert Ozkaya <ozkayamert35@gmail.com>
2024-04-20 22:37:47 +0300

    Add task

--- pizarra/task/views.py
+++ pizarra/task/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from project.models import Project
+from todolist.models import Todolist
+from .models import Task
+
+@login_required
+def add(request, project_id, todolist_id):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    todolist = Todolist.objects.filter(project=project).get(pk=todolist_id)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+
+        Task.objects.create(project=project, todolist=todolist, name=name, description=description, created_by=request.user)
+
+        return redirect(f'/projects/{project_id}/{todolist_id}/')
+
+    return render(request, 'task/add.html')
